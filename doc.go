a96005e4a07abf2300a97b96d7ad580052bc853e
Tyler Sommer <sommertm@gmail.com>
2017-11-04 00:59:00 -0600

    Update static assets

--- doc.go
+++ doc.go
@@ -5,5 +5,5 @@
 // for clients, as well as a web application.
 package motki_server
 
-//go:generate go-bindata -prefix "./public" -pkg template -tags "release" -ignore .DS_Store -o "./http/template/bindata_release.go" ./views/...
-//go:generate go-bindata -prefix "./views" -pkg assets -tags "release" -ignore .DS_Store -o "./http/module/assets/bindata_release.go" ./public/fonts/... ./public/images/ ./public/scripts/... ./public/styles/... ./public/
+//go:generate go-bindata -prefix "./views" -pkg template -tags "release" -ignore .DS_Store -o "./http/template/bindata_release.go" ./views/...
+//go:generate go-bindata -prefix "./public" -pkg assets -tags "release" -ignore .DS_Store -o "./http/module/assets/bindata_release.go" ./public/fonts/... ./public/images/ ./public/styles/... ./public/
